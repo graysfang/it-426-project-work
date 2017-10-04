8585e17836922087348c57e101555a65219a8708
graysfang <neperez23@gmail.com>
2017-10-04 08:55:25 -0700

    Start files for assignment 01.

---
+++ asgmt01_nperez/src/main/java/launcher/launcher.java
@@ -0,0 +1,12 @@
+package launcher;
+
+import javafx.application.Application;
+import layout.CalcGridLayout;
+
+public class launcher
+{
+    public static void main(String[] args)
+    {
+        Application.launch(CalcGridLayout.class, args);
+    }
+}
---
+++ asgmt01_nperez/src/main/java/layout/CalcGridLayout.java
@@ -0,0 +1,14 @@
+package layout;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalcGridLayout extends Application
+{
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+    }
+}
